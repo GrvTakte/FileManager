968f01bed8fd7890448a4ee2b0d93eb23face96d
GrvTakte <gauravtakte@gmail.com>
2017-04-28 19:09:50 +0530

    adding another three buttons

--- app/src/main/java/project/recycler/com/filehandling/MainActivity.java
+++ app/src/main/java/project/recycler/com/filehandling/MainActivity.java
@@ -3,21 +3,14 @@ package project.recycler.com.filehandling;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
 
 public class MainActivity extends Activity{
 
-    private static final int REQUEST_PICK_FILE = 1;
-    public final static String EXTRA_FILE_PATH = "file_path";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +24,11 @@ public class MainActivity extends Activity{
         Button buttonGame = (Button) findViewById(R.id.button_game);
 
 
+        Button buttonMovies = (Button) findViewById(R.id.button_movies);
+        Button buttonMusic = (Button) findViewById(R.id.button_music);
+        Button buttonNews = (Button) findViewById(R.id.button_news_paper);
 
 
-      /*  extCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/"); */
-                //intent.setDataAndType(uri, "*/*");
-               /* startActivityForResult(intent, 10);
-                Toast.makeText(getApplicationContext(),uri.toString(),Toast.LENGTH_LONG).show();
-
-                /*Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 10);
-            }
-        }); */
 
         buttonGame.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +36,7 @@ public class MainActivity extends Activity{
                 Intent i;
                 PackageManager manager = getPackageManager();
                 try {
-                    i = manager.getLaunchIntentForPackage("com.snakegame.freesnakegames");
+                    i = manager.getLaunchIntentForPackage("com.king.candycrushsaga");
                     if (i == null)
                         throw new PackageManager.NameNotFoundException();
                     i.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -85,30 +68,41 @@ public class MainActivity extends Activity{
             }
         });
 
+        buttonMovies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Uri uri  = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/Movies");
+                intent.setDataAndType(uri,"file/video");
+                startActivityForResult(intent,30);
+            }
+        });
+
+        buttonMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/Music");
+                intent.setDataAndType(uri,"file/audio");
+                startActivityForResult(intent,40);
+            }
+        });
+
+        buttonNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/NewsPaper");
+                intent.setDataAndType(uri,"file/pdf");
+                startActivityForResult(intent,50);
+            }
+        });
+
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-       /* if(resultCode == RESULT_OK && requestCode == 10){
-            try {
-                Uri uriSound = data.getData();
-               final MediaPlayer mp = new MediaPlayer();
-                mp.setDataSource(uriSound.toString());
-                mp.prepare();
-               mp.start();
-
-               /* Button myButton = new Button(this);
-                myButton.setText("Add Me");
-
-                LinearLayout ll = (LinearLayout)findViewById(R.id.buttonlayout);
-                LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-                ll.addView(myButton, lp);
-
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        } */
 
        if (resultCode == RESULT_OK && requestCode == 10){
            Uri uriPdf = data.getData();
@@ -117,23 +111,35 @@ public class MainActivity extends Activity{
            startActivity(pdfIntent);
        }
 
-        if (resultCode == RESULT_OK && requestCode == 20){
+       if (resultCode == RESULT_OK && requestCode == 20){
+           Uri uriVideo = data.getData();
+           Intent tostart = new Intent(Intent.ACTION_VIEW);
+           tostart.setDataAndType(uriVideo,"video/*");
+           startActivity(tostart);
 
-                Uri uriVideo = data.getData();
+       }
 
-                /* VideoView videoView = (VideoView) findViewById(R.id.video_view);
-                videoView.setVideoURI(uriVideo);
-                MediaController vidControl = new MediaController(this);
-                vidControl.setAnchorView(videoView);
-                videoView.setMediaController(vidControl);
-                videoView.start(); */
+       if(resultCode == RESULT_OK && requestCode== 30){
+            Uri uriVidio = data.getData();
+           Intent videoIntent = new Intent(Intent.ACTION_VIEW);
+           videoIntent.setDataAndType(uriVidio,"application/video");
+           startActivity(videoIntent);
+       }
 
-          Intent tostart = new Intent(Intent.ACTION_VIEW);
-            tostart.setDataAndType(uriVideo,"video/*");
-            startActivity(tostart);
+       if(resultCode == RESULT_OK && requestCode == 40){
+            Uri uriAudio = data.getData();
+           Intent audioIntent = new Intent(Intent.ACTION_VIEW);
+           audioIntent.setDataAndType(uriAudio,"application/audio");
+           startActivity(audioIntent);
+       }
 
+       if(resultCode == RESULT_OK && requestCode == 50){
+           Uri uriPDF = data.getData();
+           Intent pDfIntent = new Intent(Intent.ACTION_VIEW);
+           pDfIntent.setDataAndType(uriPDF,"application/pdf");
+           startActivity(pDfIntent);
+       }
 
-        }
     }
 
 }
