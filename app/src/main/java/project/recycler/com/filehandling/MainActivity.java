c0b20f2f5d4d86cdcbdee94f709a7b1b81b4d749
GrvTakte <gauravtakte@gmail.com>
2017-04-28 17:18:48 +0530

    update FileManager Application

--- app/src/main/java/project/recycler/com/filehandling/MainActivity.java
+++ app/src/main/java/project/recycler/com/filehandling/MainActivity.java
@@ -2,15 +2,14 @@ package project.recycler.com.filehandling;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
-import android.widget.MediaController;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,59 +24,55 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button fileManager = (Button) findViewById(R.id.button_file_manager);
-        Button folderManager = (Button) findViewById(R.id.button_file_manager_folder);
-        Button extCard = (Button) findViewById(R.id.button_ext_card);
+
+
         Button buttonVideo = (Button) findViewById(R.id.button_video);
+        Button buttonPdf = (Button) findViewById(R.id.button_pdf);
+        Button buttonGame = (Button) findViewById(R.id.button_game);
 
 
-        fileManager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent());
-                intent.setDataAndType(uri, "text/*");
-                startActivity(Intent.createChooser(intent, "Open folder"));
-                Toast.makeText(getApplicationContext(),uri.toString(),Toast.LENGTH_LONG).show();
-
-            }
-        });
 
 
-        folderManager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-
-                String extStore = System.getenv("EXTERNAL_STORAGE");
-                //String extStore = System.getenv("ext_card");
-
-                File f_exts = new File(extStore);
-                Uri uri = Uri.parse(extStore);
-                intent.setDataAndType(uri, "text/*");
-                startActivity(Intent.createChooser(intent, "Open"));
-                Toast.makeText(getApplicationContext(),extStore,Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-        extCard.setOnClickListener(new View.OnClickListener() {
+      /*  extCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
               Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/");
-                intent.setDataAndType(uri, "*/*");
-                startActivityForResult(intent, 10);
+                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card/"); */
+                //intent.setDataAndType(uri, "*/*");
+               /* startActivityForResult(intent, 10);
                 Toast.makeText(getApplicationContext(),uri.toString(),Toast.LENGTH_LONG).show();
 
                 /*Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 10); */
+                startActivityForResult(intent, 10);
+            }
+        }); */
+
+        buttonGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                PackageManager manager = getPackageManager();
+                try {
+                    i = manager.getLaunchIntentForPackage("com.snakegame.freesnakegames");
+                    if (i == null)
+                        throw new PackageManager.NameNotFoundException();
+                    i.addCategory(Intent.CATEGORY_LAUNCHER);
+                    startActivity(i);
+                } catch (PackageManager.NameNotFoundException e) {
+
+                }
             }
         });
 
+        buttonPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getParent()+"/ext_card");
+                intent.setDataAndType(uri,"file/pdf");
+                startActivityForResult(intent,10);
+            }
+        });
 
         buttonVideo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,7 +90,7 @@ public class MainActivity extends Activity{
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(resultCode == RESULT_OK && requestCode == 10){
+       /* if(resultCode == RESULT_OK && requestCode == 10){
             try {
                 Uri uriSound = data.getData();
                final MediaPlayer mp = new MediaPlayer();
@@ -108,12 +103,19 @@ public class MainActivity extends Activity{
 
                 LinearLayout ll = (LinearLayout)findViewById(R.id.buttonlayout);
                 LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-                ll.addView(myButton, lp); */
+                ll.addView(myButton, lp);
 
             }catch(IOException e){
                 e.printStackTrace();
             }
-        }
+        } */
+
+       if (resultCode == RESULT_OK && requestCode == 10){
+           Uri uriPdf = data.getData();
+           Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+           pdfIntent.setDataAndType(uriPdf,"application/pdf");
+           startActivity(pdfIntent);
+       }
 
         if (resultCode == RESULT_OK && requestCode == 20){
 
